2d1278dea46d0b3f7aa3721b634516ca936dd8bf
Layseiras <layseirasdev@outlook.com>
2025-04-26 11:23:07 -0300

    feat: add custom exception for qrcode generation failures

---
+++ src/main/java/dev/layseiras/qrcode/generator/exceptions/QrCodeGenerationException.java
@@ -0,0 +1,8 @@
+package dev.layseiras.qrcode.generator.exceptions;
+
+public class QrCodeGenerationException extends RuntimeException {
+    public QrCodeGenerationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
